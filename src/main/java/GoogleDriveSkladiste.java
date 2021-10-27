ffdbef1851ccbd00e74327bad2225563d5c6a15a
milosstanisavljevic <mstanisavljevic8220r@raf.rs>
2021-10-27 12:44:26 +0200

    gdrive init commit

---
+++ src/main/java/GoogleDriveSkladiste.java
@@ -0,0 +1,15 @@
+public class GoogleDriveSkladiste extends SpecifikacijaSkladista{
+
+
+    public void createRoot(String s, String s1) {
+        
+    }
+
+    public void checkIfRootExists(String s) {
+
+    }
+
+    public void checkPrivileges() {
+
+    }
+}
