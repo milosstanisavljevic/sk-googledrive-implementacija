f3ce930d00577da48ee077e1ea5b1d268fb88953
milosstanisavljevic <mstanisavljevic8220r@raf.rs>
2021-11-03 20:14:34 +0100

    google drive kredencijali, povezivanje dependensija, statik blok

--- src/main/java/GoogleDriveSkladiste.java
+++ src/main/java/GoogleDriveSkladiste.java
@@ -1,15 +1,43 @@
-public class GoogleDriveSkladiste extends SpecifikacijaSkladista{
+public class GoogleDriveSkladiste extends SpecifikacijaSkladista {
 
+    static {
 
-    public void createRoot(String s, String s1) {
-        
+        Manager.registerImpl(new GoogleDriveSkladiste());
     }
 
-    public void checkIfRootExists(String s) {
+    public boolean createRoot(String s, String s1, String s2) {
+        return false;
+    }
+
+    public boolean checkIfRootExists(String s) {
+        return false;
+    }
+
+    public void createFile(String s) {
 
     }
 
-    public void checkPrivileges() {
+    public void createMoreFiles(int i) {
+
+    }
+
+    public void createMoreFolders(int i) {
+
+    }
+
+    public void createFolder(String s) {
+
+    }
+
+    public void deleteFile(String s) {
+
+    }
+
+    public void deleteFolder(String s) {
+
+    }
+
+    public void loadUsers() {
 
     }
 }
