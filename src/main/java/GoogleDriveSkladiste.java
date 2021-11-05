693c764cb9f36d1a6990dbf57515b67df820ef94
milosstanisavljevic <mstanisavljevic8220r@raf.rs>
2021-11-06 00:21:27 +0100

    update

--- src/main/java/GoogleDriveSkladiste.java
+++ src/main/java/GoogleDriveSkladiste.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.Map;
+
 public class GoogleDriveSkladiste extends SpecifikacijaSkladista {
 
     static {
@@ -10,6 +13,81 @@ public class GoogleDriveSkladiste extends SpecifikacijaSkladista {
     }
 
     public boolean checkIfRootExists(String s) {
+        return false;
+    }
+
+    @Override
+    public void createFile(String s, String s1) {
+
+    }
+
+    @Override
+    public void createMoreFiles(String s, int i) {
+
+    }
+
+    @Override
+    public void createMoreFolders(String s, int i) {
+
+    }
+
+    @Override
+    public void createFolder(String s, String s1) {
+
+    }
+
+    @Override
+    public void deleteFile(String s, String s1) {
+
+    }
+
+    @Override
+    public void deleteFolder(String s, String s1) {
+
+    }
+
+    @Override
+    public void makeConfig(String s, Map<String, Object> map) {
+
+    }
+
+    @Override
+    public void makeDefaultConfig(String s, String s1) {
+
+    }
+
+    @Override
+    public void updateConfig(String s, int i, String s1, int i1) {
+
+    }
+
+    @Override
+    public void makeUser(String s, List<Korisnik> list) {
+
+    }
+
+    @Override
+    public void makeDefaultUser(String s, String s1, String s2) {
+
+    }
+
+    @Override
+    public void addUser(String s, String s1, String s2, String s3) {
+
+    }
+
+    @Override
+    public String checkAdmin(String s) {
+        return null;
+    }
+
+    @Override
+    public Object checkConfigType(String s, String s1) {
+        return null;
+    }
+
+    @Override
+    public boolean checkUser(String s, String s1, String s2) {
         return false;
     }
 
